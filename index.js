5b96db1ea5437c7e1a3ed53544dfb24a700b3455
abdurauf <qayumovv7007@gmail.com>
2025-12-09 10:55:46 +0500

    aaaaaaaaaaaaa

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const TelegramBot = require("node-telegram-bot-api");
 const axios = require("axios");
 
-const token = "TOKENNI_BU_YERGA_QOYING";
+const token = "8487452131:AAH7fcyKaMm9hArWWZkhpetbAVahnI7CGPQ";
 const bot = new TelegramBot(token, { polling: true });
 
 // -------------------------
